0a9a33fefc26ae7ea9ec1745842a2538b0aa1142
Michal Špaček <tupinek@gmail.com>
2013-10-18 15:31:25 +0200

    Prepare for 0.08 version.

--- DSV.pm
+++ DSV.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 
 # Version.
-our $VERSION = 0.07;
+our $VERSION = 0.08;
 
 # Constructor.
 sub new {
@@ -191,6 +191,6 @@ BSD license.
 
 =head1 VERSION
 
-0.07
+0.08
 
 =cut
